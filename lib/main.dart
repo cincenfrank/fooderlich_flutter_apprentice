518294daa0bbe295f0653d570aa84bfec33c1ba4
cincenfrank <fpetrangeli1@gmail.com>
2021-10-17 12:06:20 +0200

    converted author card in stateful widget

--- lib/main.dart
+++ lib/main.dart
@@ -8,13 +8,10 @@ void main() {
 }
 
 class Fooderlich extends StatelessWidget {
-  // 2
   const Fooderlich({Key? key}) : super(key: key);
   @override
   Widget build(BuildContext context) {
     final theme = FooderlichTheme.dark();
-    // TODO: Apply Home widget
-    // 3
     return MaterialApp(
       theme: theme,
       title: 'Fooderlich',
